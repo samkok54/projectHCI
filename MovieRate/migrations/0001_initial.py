461e29b1ecbdd95a334905cb145b12c2beeda2af
poo <you@example.com>
2015-04-23 12:54:26 +0700

    เพิ่ม pep8

--- MovieRate/migrations/0001_initial.py
+++ MovieRate/migrations/0001_initial.py
@@ -14,36 +14,29 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Comment',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
                 ('user', models.TextField(default='')),
                 ('comment_text', models.TextField(default='')),
-                ('date', models.DateTimeField(blank=True, default=datetime.datetime.now)),
+                ('date', models.DateTimeField(default=datetime.datetime.now, blank=True)),
                 ('like', models.IntegerField(default=0)),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Movie',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
                 ('name', models.TextField(default='')),
                 ('detail', models.TextField(default='')),
                 ('release_date', models.TextField(default='')),
                 ('rate', models.FloatField(default=0)),
                 ('viewer', models.IntegerField(default=0)),
                 ('poster', models.URLField(default='')),
-                ('add_date', models.DateTimeField(blank=True, default=datetime.datetime.now)),
+                ('add_date', models.DateTimeField(default=datetime.datetime.now, blank=True)),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.AddField(
             model_name='comment',
             name='movie',
-            field=models.ForeignKey(to='MovieRate.Movie', default=None),
-            preserve_default=True,
+            field=models.ForeignKey(default=None, to='MovieRate.Movie'),
         ),
     ]
